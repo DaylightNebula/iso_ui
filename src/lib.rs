200cbfc0f7a590d8dc0b025643f8c3234d969b8d
DaylightNebula <daylightnebula1@gmail.com>
2026-07-12 22:07:58 -0600

    Fixed resize bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(linked_list_cursors)]
 
 use anarchy::{Query, Res, ResMut, macros::{Getters, Resource, system}};
-use cell::{App, Frame, Graphics, Plugin};
+use cell::{App, Frame, Graphics, Plugin, WindowDimensions};
 use magician_vgpu::{Buffer, LoadOp, MutableBuffer, PassAttachment, PassTarget, Pipeline, ShaderSource, ShaderType, StoreOp, glam::Vec2};
 use mutual::CowData;
 
@@ -234,19 +234,20 @@ fn init_resources(
 fn ui_render_pass(
     graphics: Res<Graphics>,
     frame: ResMut<Frame>,
-    resources: Res<UIRenderResources>
+    resources: Res<UIRenderResources>,
+    window_dimensions: Res<WindowDimensions>
 ) {
     // create UI elements
     let nodes = Query::<&UINodeSDFRoot>::new(world.database())
         .as_iter()
         .map(|a| a.clone())
         .collect::<Vec<_>>();
-    let elements = layout_ui_nodes(&nodes, [800.0, 600.0]);
+    let elements = layout_ui_nodes(&nodes, [window_dimensions.x as f32, window_dimensions.y as f32]);
 
     // create root element
     let root = SDFElement { 
-        center: Vec2::new(400.0, 300.0), 
-        dimensions: Vec2::new(800.0, 600.0), 
+        center: Vec2::new(window_dimensions.x as f32 / 2.0, window_dimensions.y as f32 / 2.0), 
+        dimensions: Vec2::new(window_dimensions.x as f32, window_dimensions.y as f32), 
         children: elements, 
         ..Default::default() 
     };
@@ -270,43 +271,3 @@ fn ui_render_pass(
     pass.bind_raw(0, resources.bind_group());
     pass.pass_mut().draw(0..3, 0..1);
 }
-
-// /// Temporary demo tree used until a real UI tree source is wired in.
-// fn test_tree() -> anyhow::Result<SDFElement> {
-//     let mut children = LinkedList::new();
-    
-//     let font_data = include_bytes!("../examples/LiberationSans-Regular.ttf");
-//     let mut font = SDFFont::new(font_data)?;
-//     let string = font.render_glyph_line("Hello World!@#%^&", 18.0)?;
-//     let mut cur_x = (string.dimensions.x / -2.0) + 390.0;
-//     string.chars.into_iter().for_each(|entry| {
-//         if let Some(shape) = entry.shape {
-//             cur_x += entry.advance;
-//             children.push_back(SDFElement {
-//                 center: Vec2::new(cur_x - (entry.advance / 2.0), 300.0),
-//                 dimensions: entry.dimensions,
-//                 style: SDFStyle { primary_color: Vec4::ONE, border_color: Vec4::ONE, border_width: 0.0 },
-//                 shape,
-//                 children: LinkedList::new(),
-//                 ..Default::default()
-//             });
-//         } else {
-//             cur_x += entry.advance;
-//             // None
-//         }
-//     });
-
-//     Ok(SDFElement { 
-//         center: Vec2::new(400.0, 300.0), 
-//         dimensions: Vec2::new(200.0, 200.0),
-//         style: SDFStyle { 
-//             primary_color: Vec4::new(1.0, 0.0, 0.0, 1.0), 
-//             border_color: Vec4::new(0.0, 0.0, 0.0, 1.0), 
-//             border_width: 3.0
-//         }, 
-//         shape: SDFShape::Rectangle(SDFRectangle { radii: Vec4::new(15.0, 15.0, 15.0, 15.0) }), 
-//         // shape: SDFShape::Empty,
-//         children: children,
-//         ..Default::default()
-//     })
-// }
